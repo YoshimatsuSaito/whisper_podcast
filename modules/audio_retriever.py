288ca8053c77a6d616e1353376bbfc6d53a4c5c4
YoshimatsuSaito <middle158725071@gmail.com>
2023-11-04 04:04:03 +0000

    add function to chunk audio

--- modules/audio_retriever.py
+++ modules/audio_retriever.py
@@ -1,9 +1,13 @@
 import os
 from email.utils import parsedate_to_datetime
+from pathlib import Path
 
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup, Tag
+from pydub import AudioSegment
+
+TEN_MINUTES = 10 * 60 * 1000
 
 class PodcastRetriever:
     """Retrieves the podcast audio from the given url"""
@@ -22,7 +26,7 @@ class PodcastRetriever:
         else:
             return res.text
 
-    def parse_data(self) -> None:
+    def _parse_data(self) -> None:
         """Returns a dataframe with the data from the podcast"""
         r = requests.get(self.url)
         soup = BeautifulSoup(r.content, 'xml')
@@ -33,7 +37,7 @@ class PodcastRetriever:
                 dict_data[tag].append(self._safe_find(item, tag))
         self.df_podcast = pd.DataFrame(dict_data)
 
-    def format_data(self) -> None:
+    def _format_data(self) -> None:
         """Formats the data from the dataframe"""
         if self.df_podcast is None:
             raise Exception("You must first parse the data using parse_data()")
@@ -48,7 +52,12 @@ class PodcastRetriever:
         except:
             pass
     
-    def download_audio(self, output_dir: str, idx: int) -> str:
+    def parse_and_format_data(self) -> None:
+        """Parses and formats the data"""
+        self._parse_data()
+        self._format_data()
+    
+    def _download_audio(self, output_dir: Path, idx: int) -> Path:
         """Downloads the audio from the podcast"""
         if self.df_podcast is None:
             raise Exception("You must first parse the data using parse_data()")
@@ -56,13 +65,41 @@ class PodcastRetriever:
         ser_target = self.df_podcast.iloc[idx]
 
         r = requests.get(ser_target['enclosure'])
-        output_path = f"{output_dir}/{ser_target['title']}.mp3"
+        output_path = output_dir / f"{ser_target['title']}_{ser_target['pubDate'].strftime('%Y%m%d%H%M')}.mp3"
         with open(output_path, "wb") as f:
             f.write(r.content)
         
         return output_path
 
-    def delete_audio(self, file_path_todelete: str) -> None:
-        """Deletes the audio file if it is mp3 file"""
-        if os.path.exists(file_path_todelete) and file_path_todelete.endswith(".mp3"):
-            os.remove(file_path_todelete)
+    def _chunk_audio(self, file_path_to_chunk: Path) -> Path:
+        """Chunks the audio file into 10 minute chunks"""
+        if file_path_to_chunk.exists() and file_path_to_chunk.suffix == '.mp3':
+            title_audio = file_path_to_chunk.name.split('.')[0]
+            output_dir = file_path_to_chunk.parent / title_audio
+            os.makedirs(output_dir, exist_ok=True)
+
+            audio_segment = AudioSegment.from_mp3(file_path_to_chunk)
+            for i, chunk in enumerate(audio_segment[::TEN_MINUTES]):
+                idx = i if i >= 10 else f"0{i}"
+                chunk.export(f"{output_dir}/audio_{idx}.mp3", format="mp3")
+            
+            # delete the original file
+            os.remove(file_path_to_chunk)
+
+            return output_dir
+    
+    def download_and_chunk_audio(self, output_dir: Path, idx: int) -> None:
+        """Downloads and chunks the audio file"""
+        if self.df_podcast is None:
+            raise Exception("You must first parse the data using parse_data()")
+        file_path_to_chunk = self._download_audio(output_dir, idx)
+        chunk_dir = self._chunk_audio(file_path_to_chunk)
+
+        return chunk_dir
+    
+    def download_and_chunk_all_audio(self, output_dir: Path) -> None:
+        """Downloads and chunks all the audio files"""
+        if self.df_podcast is None:
+            raise Exception("You must first parse the data using parse_data()")
+        for idx in self.df_podcast.index:
+            self.download_and_chunk_audio(output_dir, idx)
--- modules/transcriber.py
+++ modules/transcriber.py
@@ -1,10 +1,20 @@
+from pathlib import Path
+
 import dotenv
 import openai
+
 
 dotenv.load_dotenv()
 
 
-def transcribe(audio_file_path: str, model_name: str = "whisper-1", language: str = "en") -> str:
+
+def transcribe(audio_file_path: Path, model_name: str = "whisper-1", language: str = "en") -> str:
     """Transcribes the audio file using the OpenAI API"""
-    transcript = openai.Audio.transcribe(model_name, audio_file_path, verbose=True, language=language)
-    return transcript
+    audio_file = open(audio_file_path, "rb")
+    transcript = openai.Audio.transcribe(model_name, audio_file, verbose=True, language=language)
+    return transcript["text"]
+
+
+
+
+
